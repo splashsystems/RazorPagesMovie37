fe223bf1a257659e4b675ab29d9099f75701f972
Scot <bscot.hagen@gmail.com>
2021-08-18 11:20:12 -0700

    fixed search. added migration for data annotations

--- RazorPagesMovie37/Migrations/20210818181737_New_DataAnnotations.Designer.cs
+++ RazorPagesMovie37/Migrations/20210818181737_New_DataAnnotations.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovie37.Data;
 
 namespace RazorPagesMovie37.Migrations
 {
     [DbContext(typeof(RazorPagesMovie37Context))]
-    partial class RazorPagesMovie37ContextModelSnapshot : ModelSnapshot
+    [Migration("20210818181737_New_DataAnnotations")]
+    partial class New_DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
